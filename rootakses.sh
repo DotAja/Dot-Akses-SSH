a5f0bd27eac7531f1a3070922e861bf9a57eee55
DotAja <142836390+DotAja@users.noreply.github.com>
2024-01-27 18:58:28 +0700

    Update rootakses.sh

--- rootakses.sh
+++ rootakses.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
 sudo su
-echo -e "dot\ndot" | passwd root
+echo -e "$123\n$123\n" | sudo passwd
 wget -qO /etc/ssh/sshd_config https://raw.githubusercontent.com/DotAja/Dot-Akses-SSH/main/sshd_config
 systemctl restart sshd
-public_ip=$(curl -s ifconfig.me)
+public_ip=$(curl -Ls http://ipinfo.io/ip)
 echo "===================================="
 printf "%-30s\n" "DOT AJA"
 echo "===================================="
