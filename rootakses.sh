1e9dbb59ae9c1c0feb1fca506f74e6477f34fbfa
DotAja <142836390+DotAja@users.noreply.github.com>
2024-01-27 19:18:38 +0700

    Update rootakses.sh

--- rootakses.sh
+++ rootakses.sh
@@ -1,16 +1,18 @@
-#!/bin/sh
+#!/bin/bash
 
-sudo su
-echo -e "$123\n$123\n" | sudo passwd
-wget -qO /etc/ssh/sshd_config https://raw.githubusercontent.com/DotAja/Dot-Akses-SSH/main/sshd_config
-systemctl restart sshd
-public_ip=$(curl -Ls http://ipinfo.io/ip)
+# Ganti kata sandi root
+echo -e "Masukkan kata sandi baru untuk root:"
+read -s new_password
+echo -e "$new_password\n$new_password" | passwd root
+
+# Dapatkan IP publik
+public_ip=$(curl -s ifconfig.me)
+
+# Tampilkan informasi
 echo "===================================="
-printf "%-30s\n" "DOT AJA"
+echo "Informasi Akses"
 echo "===================================="
-echo "AKSES SSH : ssh@$public_ip:22"
-echo "PASSWORD : dot"
+echo "IP Publik: $public_ip"
+echo "Kata Sandi Root: $new_password"
 echo "===================================="
-echo "AKSES SSH"
-echo "COPY DAN PASTEKAN KE PUTTY"
-echo "===================================="
+
