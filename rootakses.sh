25e7bda46dabf02a80de807c2e55110d0a58a21e
DotAja <142836390+DotAja@users.noreply.github.com>
2024-05-11 01:53:42 +0700

    Update rootakses.sh

--- rootakses.sh
+++ rootakses.sh
@@ -1,23 +1,21 @@
 #!/bin/bash
 
-# Ganti kata sandi root
 echo -e "dot\ndot" | passwd root
 
-# Unduh konfigurasi SSH dari URL
 config_url="https://raw.githubusercontent.com/DotAja/Dot-Akses-SSH/main/sshd_config"
+
 wget -O /etc/ssh/sshd_config $config_url
 
-# Restart layanan SSH
 systemctl restart sshd
 
-# Dapatkan IP publik
 public_ip=$(curl -s ifconfig.me)
 
-# Tampilkan informasi
 clear
 echo "===================================="
 echo "DOT AJA"
 echo "===================================="
-echo "IP Akses SSH:ssh root@$public_ip -p 22"
-echo "Kata Sandi: dot"
+echo "USER : root"
+echo "IP AKSES : $public_ip
+echo "PASSWORD : dot"
+echo "SSH CMD: ssh root@$public_ip -p 22"
 echo "===================================="
